5a668895e7e12c0e157f083f94de3de48ad35649
dylan wuu <dylanwuu@oulook.com>
2023-06-29 11:07:38 -0700

    exercise completed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace MethodOverloading
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static string add(int x, int y, bool dollar)
+        {
+            int sum = x + y;
+            if (dollar == true)
+            {
+                if (sum == 1 || sum == -1)
+                {
+                    return $"{sum} dollar";
+                }
+                else
+                {
+                    return $"{sum} dollars";
+                }
+            }
+            else
+            {
+                return $"{sum}";
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(add(1, 2));
+            Console.WriteLine(add(1.1, 2.0));
+            Console.WriteLine(add(1, 0, true));
         }
     }
 }
